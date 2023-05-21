3e34a7c2f6c5c68b2ff872adf0d5ff075dd476c8
Minky Baek <55613446+toyo30@users.noreply.github.com>
2023-05-22 01:40:23 +0900

    add: store.rules'

--- src/pages/comment/Comment.tsx
+++ src/pages/comment/Comment.tsx
@@ -5,8 +5,6 @@ import { ImgCard } from "../../components/imgCard/ImgCard";
 import { db } from "../../firebase";
 import { sortByLike, sortByTime } from "../../utils/listSort/list-sort";
 import * as S from "./CommentStyles";
-import Lottie from "lottie-react";
-import loadingLottie from "../../lotties/loading.json";
 
 export const Comment = () => {
   const [eventDocuments, setEventDocuments] = useState<any[]>([]);
@@ -33,7 +31,6 @@ export const Comment = () => {
     return () => {
       unsubscribe();
     };
-    setLoading(false);
   }, []); // 의존성 배열에 빈 배열 사용
 
   const handleClickLike = () => {
@@ -45,12 +42,7 @@ export const Comment = () => {
   };
 
   return (
-    {loading ? (
-      <S.CommentContainer>
-        <Lottie animationData={loadingLottie} />
-      </S.CommentContainer>
-    ):(
-      <S.CommentContainer>
+    <S.CommentContainer>
       <div style={{ padding: "10px 20px 10px", textAlign: "right" }}>
         <Button
           variant={`${likeSort ? "contained" : "outlined"}`}
@@ -71,39 +63,47 @@ export const Comment = () => {
           최신순
         </Button>
       </div>
-      {eventDocuments.length > 0 ? (likeSort
-        ? sortByLike(eventDocuments).map((item: any) => (
-            <>
-              <ImgCard
-                key={item.id}
-                id={item.id}
-                url={item.url}
-                userId={item.userId}
-                password={item.password}
-                text={item.text}
-                likeCount={item.like}
-                location={item.location}
-              />
-            </>
-          ))
-        : sortByTime(eventDocuments).map((item: any) => (
-            <>
-              <ImgCard
-                key={item.id}
-                id={item.id}
-                url={item.url}
-                userId={item.userId}
-                password={item.password}
-                text={item.text}
-                likeCount={item.like}
-                location={item.location}
-              />
-            </>
-          ))):<img src={`${process.env.PUBLIC_URL}/Zola.png`}></img>
-    </S.CommentContainer>)}
+      {eventDocuments.length > 0 &&
+        (likeSort
+          ? sortByLike(eventDocuments).map((item: any) => (
+              <>
+                <ImgCard
+                  key={item.id}
+                  id={item.id}
+                  url={item.url}
+                  userId={item.userId}
+                  password={item.password}
+                  text={item.text}
+                  likeCount={item.like}
+                  location={item.location}
+                />
+              </>
+            ))
+          : sortByTime(eventDocuments).map((item: any) => (
+              <>
+                <ImgCard
+                  key={item.id}
+                  id={item.id}
+                  url={item.url}
+                  userId={item.userId}
+                  password={item.password}
+                  text={item.text}
+                  likeCount={item.like}
+                  location={item.location}
+                />
+              </>
+            )))}
+    </S.CommentContainer>
   );
 };
 
+{
+  /* {loading ? (
+      <S.CommentContainer>
+        <Lottie animationData={loadingLottie} />
+      </S.CommentContainer>
+    ):( */
+}
 
 // {loading ? (
 //   <Lottie animationData={loadingLottie} />
@@ -111,44 +111,44 @@ export const Comment = () => {
 
 // :  <img src={`${process.env.PUBLIC_URL}/Zola.png`}></img>
 
-  // useEffect(() => {
-  //   const collectionNames = Object.keys(pins); // 각 컬렉션의 이름을 담은 배열
+// useEffect(() => {
+//   const collectionNames = Object.keys(pins); // 각 컬렉션의 이름을 담은 배열
 
-  //   const fetchData = async () => {
-  //     let allData: string[] = [];
+//   const fetchData = async () => {
+//     let allData: string[] = [];
 
-  //     // Promise.all을 이용하여 모든 컬렉션의 데이터를 병렬로 가져옵니다.
-  //     // await Promise.all(
-  //     //   collectionNames.map(async (collectionName) => {
-  //     //     const collectionRef = collection(db, collectionName);
-  //     //     console.log(collectionRef, "collectionRef");
+//     // Promise.all을 이용하여 모든 컬렉션의 데이터를 병렬로 가져옵니다.
+//     // await Promise.all(
+//     //   collectionNames.map(async (collectionName) => {
+//     //     const collectionRef = collection(db, collectionName);
+//     //     console.log(collectionRef, "collectionRef");
 
-  //     //     // 실시간 리스너 설정
-  //     //     const unsubscribe = onSnapshot(collectionRef, (querySnapshot) => {
-  //     //       const newEventDocuments: any[] = [];
-  //     //       querySnapshot.forEach((doc) => {
-  //     //         console.log(doc.data(), "doc.data()");
-  //     //         if (doc.data()) {
-  //     //           newEventDocuments.push({
-  //     //             ...doc.data(),
-  //     //             id: doc.id,
-  //     //           });
-  //     //         }
-  //     //       });
+//     //     // 실시간 리스너 설정
+//     //     const unsubscribe = onSnapshot(collectionRef, (querySnapshot) => {
+//     //       const newEventDocuments: any[] = [];
+//     //       querySnapshot.forEach((doc) => {
+//     //         console.log(doc.data(), "doc.data()");
+//     //         if (doc.data()) {
+//     //           newEventDocuments.push({
+//     //             ...doc.data(),
+//     //             id: doc.id,
+//     //           });
+//     //         }
+//     //       });
 
-  //     //       allData = [...allData, ...newEventDocuments];
-  //     //       setEventDocuments([...eventDocuments, ...newEventDocuments]);
-  //     //     });
+//     //       allData = [...allData, ...newEventDocuments];
+//     //       setEventDocuments([...eventDocuments, ...newEventDocuments]);
+//     //     });
 
-  //     //     // 컴포넌트가 언마운트되면 실시간 리스너 해제
-  //     //     return () => {
-  //     //       unsubscribe();
-  //     //     };
-  //     //   })
-  //     // );
+//     //     // 컴포넌트가 언마운트되면 실시간 리스너 해제
+//     //     return () => {
+//     //       unsubscribe();
+//     //     };
+//     //   })
+//     // );
 
-  //     // setEventDocuments(allData);
-  //   };
+//     // setEventDocuments(allData);
+//   };
 
-  //   fetchData();
-  // }, []);
+//   fetchData();
+// }, []);
